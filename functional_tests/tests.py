c7fd4f405e3532d4fa391fd6c23faadad56773b0
nature1995 <gzr2008@vip.qq.com>
2019-04-17 12:14:42 -0400

    Fix tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,9 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import WebDriverException
 import unittest
 import time
-
-MAX_WAIT = 10
+import os
 
 
 class TestCase(LiveServerTestCase):
@@ -16,7 +14,6 @@ class TestCase(LiveServerTestCase):
         # self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.driver.maximize_window()
         self.base_url = self.live_server_url
 
     def test_include_items(self):
@@ -28,66 +25,42 @@ class TestCase(LiveServerTestCase):
             self.assertIn(item, product[0].text)
 
     def test_gesture_detection(self):
-        start_time = time.time()
-        while True:
-            try:
-                self.driver.get(self.base_url)
-                self.driver.find_element_by_xpath('/html/body/main/section[1]/p[1]/a[4]').click()
-                self.driver.find_element_by_name("image_file").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/Gesture7.jpg')
-                self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
-                time.sleep(3)
-                gesture_res = self.driver.find_element_by_xpath('//*[@id="showgesture"]')
-                self.assertIn('Gesture: Hand open', gesture_res.text)
-                return
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        self.driver.get(self.base_url)
+        self.driver.find_element_by_xpath('/html/body/main/section[1]/p[1]/a[4]').click()
+        self.driver.find_element_by_name("image_file").send_keys(os.path.abspath('.') + '/functional_tests/images/Gesture7.jpg')
+        self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
+        time.sleep(5)
+        gesture_res = self.driver.find_element_by_xpath('//*[@id="showgesture"]')
+        self.assertIn('Gesture: Hand open', gesture_res.text)
 
     def test_bank_card_detection(self):
-        start_time = time.time()
-        while True:
-            try:
-                self.driver.get(self.base_url)
-                self.driver.find_element_by_xpath('/html/body/main/section[1]/p[1]/a[2]').click()
-                self.driver.find_element_by_name("image_file").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/card3.png')
-                self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
-                time.sleep(5)
-                bank_card_res = self.driver.find_element_by_xpath('//*[@id="shownumber"]')
-                self.assertIn('Card number: 4000123456789123', bank_card_res.text)
-                return
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        self.driver.get(self.base_url)
+        self.driver.find_element_by_xpath('/html/body/main/section[1]/p[1]/a[2]').click()
+        self.driver.find_element_by_name("image_file").send_keys(os.path.abspath('.') + '/functional_tests/images/card3.png')
+        self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
+        time.sleep(5)
+        bank_card_res = self.driver.find_element_by_xpath('//*[@id="shownumber"]')
+        self.assertIn('Card number: 4000123456789123', bank_card_res.text)
 
     def test_face_compare_detection(self):
-        start_time = time.time()
-        while True:
-            try:
-                self.driver.get(self.base_url)
-                self.driver.find_element_by_xpath('/html/body/main/section[1]/p[1]/a[1]').click()
-                self.driver.find_element_by_name("image_file1").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/campare1.jpg')
-                self.driver.find_element_by_name("image_file2").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/campare2.jpg')
-                self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
-                time.sleep(5)
-                face_compare_res = self.driver.find_element_by_xpath('//*[@id="showconfidence"]')
-                self.assertIn('Similarity：85', face_compare_res.text)
-                return
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        self.driver.get(self.base_url)
+        self.driver.find_element_by_xpath('/html/body/main/section[1]/p[1]/a[1]').click()
+        self.driver.find_element_by_name("image_file1").send_keys(os.path.abspath('.') + '/functional_tests/images/campare1.jpg')
+        self.driver.find_element_by_name("image_file2").send_keys(os.path.abspath('.') + '/functional_tests/images/campare2.jpg')
+        self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
+        time.sleep(5)
+        face_compare_res = self.driver.find_element_by_xpath('//*[@id="showconfidence"]')
+        self.assertIn('Similarity：85', face_compare_res.text)
 
     def test_identification_detection(self):
         self.driver.get(self.base_url+'/predict/')
         time.sleep(10)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/fox.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/fox.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         r50_res_1 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
         self.assertIn('Prediction: red_fox', r50_res_1.text)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/panda0.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/panda0.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         r50_res_2 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
@@ -95,12 +68,12 @@ class TestCase(LiveServerTestCase):
 
         self.driver.get(self.base_url+'/predict_MobileNet/')
         time.sleep(10)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/fox.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/fox.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         mn_res_1 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
         self.assertIn('Prediction: red_fox', mn_res_1.text)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/panda0.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/panda0.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         mn_res_2 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
@@ -108,12 +81,12 @@ class TestCase(LiveServerTestCase):
 
         self.driver.get(self.base_url+'/predict_MobileNetV2/')
         time.sleep(10)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/fox.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/fox.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         mn2_res_1 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
         self.assertIn('Prediction: grey_fox', mn2_res_1.text)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/panda0.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/panda0.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         mn2_res_2 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
@@ -121,12 +94,12 @@ class TestCase(LiveServerTestCase):
 
         self.driver.get(self.base_url+'/predict_Xception/')
         time.sleep(10)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/fox.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/fox.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         mn2_res_1 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
         self.assertIn('Prediction: grey_fox', mn2_res_1.text)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/panda0.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/panda0.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         mn2_res_2 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
@@ -134,12 +107,12 @@ class TestCase(LiveServerTestCase):
 
         self.driver.get(self.base_url+'/predict_VGG16/')
         time.sleep(10)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/fox.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/fox.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         vgg16_res_1 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
         self.assertIn('Prediction: red_fox', vgg16_res_1.text)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/panda0.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images//panda0.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         vgg16_res_2 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
@@ -147,12 +120,12 @@ class TestCase(LiveServerTestCase):
 
         self.driver.get(self.base_url+'/predict_VGG19/')
         time.sleep(10)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/fox.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/fox.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         vgg19_res_1 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
         self.assertIn('Prediction: red_fox', vgg19_res_1.text)
-        self.driver.find_element_by_name("image").send_keys('/Users/ziran/Develop/Git_file/AI-plateform-server/functional_tests/images/panda0.jpg')
+        self.driver.find_element_by_name("image").send_keys(os.path.abspath('.') + '/functional_tests/images/panda0.jpg')
         self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/button').click()
         time.sleep(5)
         vgg19_res_2 = self.driver.find_element_by_xpath('//*[@id="showprediction"]')
@@ -165,10 +138,10 @@ class TestCase(LiveServerTestCase):
 if __name__ == '__main__':
     # Test suite
     suite = unittest.TestSuite()
-    # suite.addTest(TestCase("test_include_items"))
-    # suite.addTest(TestCase("test_gesture_detection"))
-    # suite.addTest(TestCase("test_bank_card_detection"))
-    # suite.addTest(TestCase("test_face_compare_detection"))
+    suite.addTest(TestCase("test_include_items"))
+    suite.addTest(TestCase("test_gesture_detection"))
+    suite.addTest(TestCase("test_bank_card_detection"))
+    suite.addTest(TestCase("test_face_compare_detection"))
     suite.addTest(TestCase("test_identification_detection"))
     # Run test
     runner = unittest.TextTestRunner()
