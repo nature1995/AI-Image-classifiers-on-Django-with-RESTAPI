c4e63a3f1db4d409bdd00a609ce146b031a51b5d
nature1995 <gzr2008@vip.qq.com>
2019-04-17 15:31:36 -0400

    Update

--- apps/tenc_ai/views.py
+++ apps/tenc_ai/views.py
@@ -18,33 +18,25 @@ def tenc_ai(request):
 
 def curlmd5(src):
     m = hashlib.md5(src.encode('UTF-8'))
-    # 将得到的MD5值所有字符转换成大写
     return m.hexdigest().upper()
 
 
 def get_params2(plus_item):
     global params
-    #请求时间戳（秒级），用于防止请求重放（保证签名5分钟有效）  
     t = time.time()
     time_stamp=str(int(t))
-    # 请求随机字符串，用于保证签名不可预测  
     nonce_str = ''.join(random.sample(string.ascii_letters + string.digits, 10))
-    # 应用标志，这里修改成自己的id和key  
     app_id='2111918951'
     app_key='oGryHSSYbDTnvCSJ'
-    params = {'app_id':app_id,
-              'text':plus_item,
-              'time_stamp':time_stamp,
-              'nonce_str':nonce_str,
+    params = {'app_id': app_id,
+              'text': plus_item,
+              'time_stamp': time_stamp,
+              'nonce_str': nonce_str,
              }
     sign_before = ''
-    #要对key排序再拼接
     for key in sorted(params):
-        # 键值拼接过程value部分需要URL编码，URL编码算法用大写字母，例如%E8。quote默认大写。
         sign_before += '{}={}&'.format(key,quote(params[key], safe=''))
-    # 将应用密钥以app_key为键名，拼接到字符串sign_before末尾
     sign_before += 'app_key={}'.format(app_key)
-    # 对字符串sign_before进行MD5运算，得到接口请求签名  
     sign = curlmd5(sign_before)
     params['sign'] = sign
     return params
@@ -52,12 +44,9 @@ def get_params2(plus_item):
 
 def get_params1(plus_item):
     global params
-    # 请求时间戳（秒级），用于防止请求重放（保证签名5分钟有效）  
     t = time.time()
     time_stamp = str(int(t))
-    # 请求随机字符串，用于保证签名不可预测  
     nonce_str = ''.join(random.sample(string.ascii_letters + string.digits, 10))
-    # 应用标志，这里修改成自己的id和key  
     app_id = '2111918951'
     app_key = 'oGryHSSYbDTnvCSJ'
     params = {'app_id': app_id,
@@ -67,13 +56,9 @@ def get_params1(plus_item):
               'session': '10000'
               }
     sign_before = ''
-    # 要对key排序再拼接
     for key in sorted(params):
-        # 键值拼接过程value部分需要URL编码，URL编码算法用大写字母，例如%E8。quote默认大写。
         sign_before += '{}={}&'.format(key, quote(params[key], safe=''))
-    # 将应用密钥以app_key为键名，拼接到字符串sign_before末尾
     sign_before += 'app_key={}'.format(app_key)
-    # 对字符串sign_before进行MD5运算，得到接口请求签名  
     sign = curlmd5(sign_before)
     params['sign'] = sign
     return params
@@ -81,9 +66,7 @@ def get_params1(plus_item):
 
 def get_content(plus_item):
     global payload, r
-    # 聊天的API地址 
     url = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat"
-    # 获取请求参数  
     plus_item = plus_item.POST.get('text1', None)
     plus_item = plus_item.encode('utf-8')
     payload = get_params1(plus_item)
@@ -93,8 +76,8 @@ def get_content(plus_item):
 
 
 def get_sentiments(request):
-    if request.POST:  # 如果数据提交
-         print('有提交')
+    if request.POST:
+         print('Having submit')
     url = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textpolar"
     comments = request.POST.get('text', None)
     comments = comments.encode('utf-8')
